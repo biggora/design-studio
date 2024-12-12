7f69b9cf129e2fbf7eccfda972769ccbdbb42188
biggora <aleksej@gordejev.lv>
2024-12-12 09:54:39 +0200

    add images rule for *.redbubble.com

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,12 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "*.redbubble.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
         hostname: "*.redbubble.net",
         port: "",
         pathname: "/**",
